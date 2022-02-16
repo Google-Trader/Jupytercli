4038d9b6ab3b90807d2c78e6962a49418c69d039
GlobalDev <98990121+langsgit@users.noreply.github.com>
2022-02-16 07:30:52 +0200

    Create __iox__.py
    iox skelaton

---
+++ __iox__.py
@@ -0,0 +1,69 @@
+## __iox__: Operating-System with Tradeable-Platform based on PayPal-Account and Cubical-Bank as well Rufusles-Security by AsusTech.org and Xioami.org
+def __AutomatedStrategies__():
+  pass
+
+def __BarsType__():
+  pass
+
+def __Indicarors__():
+  pass
+
+def __Indexes__():
+  pass
+
+def __PayPalVendoring__():
+  pass
+
+def __Atom__():
+  pass
+
+def __GitHubBoards__():
+  pass
+
+def __RufusCybersecurityStore__():
+  pass
+
+def __AliExpressConnection__():
+  pass
+
+def __AliExpressFinancialStors__():
+  pass
+
+def __XioamiStudio__():
+  pass
+
+def __CubiaclConnection__():
+  pass
+
+def __CanaryConnection__():
+  pass
+
+def __AsusCloud__():
+  pass
+
+def __XioamiNostros__():
+  print('Automated or SemiAutomated')
+  pass
+
+def __AsusOffices__():
+  pass
+
+def __RogComputers__():
+  pass
+
+def __ProArt__():
+  print('Exclusive to Enterprises')
+  pass
+
+def __JupyterNotebook__():
+  print('iox terminal')
+  pass
+
+def __RogCommunications__():
+  pass
+
+def __XioamiPro__():
+  pass
+
+def __XioamiFirewall__():
+  pass
